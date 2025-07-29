4bb49d5aaa738960e5944c789b9da9c4cb1d6fcf
Ben <benjamimlucasrodrigues@gmail.com>
2025-07-28 23:55:42 -0300

    Adding drinks

--- restaurant_script.py
+++ restaurant_script.py
@@ -1,9 +1,13 @@
 #Initial values of the variables
 
-orders = {}
+orders_plate = {}
+orders_drink = {}
 plate_number = 0
+drink_number = 0
 
 # Taking orders from the user
+
+# Plates
 table_number = input("Table number: ")
 a_plate = input("Type 1 to order a plate or 0 to procced: ")
 while a_plate != "0":
@@ -11,14 +15,33 @@ while a_plate != "0":
         plate_number = plate_number + 1
         a_plate = '0'
         main_course = input("Main course: ")
-        garnishes = input("Garnishes: ")
-        orders [plate_number] = [main_course, garnishes]
+        garnishes_plate = input("Garnishes: ")
+        orders_plate [plate_number] = [main_course, garnishes_plate]
     a_plate = input("Type 1 to order a plate or 0 to procced: ")
+
+# Drinks
+a_drink = input("Type 1 to order a drink or 0 to procced: ")
+while a_drink != "0":
+    if a_drink == '1':
+        drink_number = drink_number + 1
+        a_drink = '0'
+        drink = input("Drink: ")
+        garnishes_drink = input("Garnishes: ")
+        orders_drink [drink_number] = [drink, garnishes_drink]
+    a_drink = input("Type 1 to order a drink or 0 to procced: ")
+
+# Displaying the orders
+print("\n")
 
 print(f"Orders for table {table_number}:")
 
-for plate_number, order in orders.items():
+print()
+for plate_number, order in orders_plate.items():
     print(f"Plate {plate_number}: {order}")
 
+for drink_number, order in orders_drink.items():
+    print(f"Drink {drink_number}: {order}")
+
+print()
+
 print("Orders have been taken successfully.")
-# Displaying the orders in a structured format
